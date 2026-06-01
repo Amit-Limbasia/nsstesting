e11f6e65226467dfc39271e128315b5f2713be8e
sahil50303 <165477826+sahil50303@users.noreply.github.com>
2026-06-01 18:23:39 +0530

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -461,7 +461,7 @@ with st.sidebar:
     st.markdown('</div>', unsafe_allow_html=True)
     
     # Fixed Backend API URL (hidden from UI)
-    api_url = "https://chatbot-code-scaz.onrender.com/message"
+    api_url = "https://sahil-j6bm.onrender.com/message"
 
     # Stats
     st.markdown('<div class="info-box">', unsafe_allow_html=True)
